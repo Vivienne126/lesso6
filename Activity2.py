43a708b2dc26e07746dc96b8adee9aff68c3df59
Vivienne126 <vivienne.singh@cns.ac.in>
2026-04-07 22:44:25 +0530

    New

---
+++ Activity2.py
@@ -0,0 +1,17 @@
+def sieveoferasthoses(num):
+    prime=[True for i in range(num+1)]
+    p=2
+    while (p*p<=num):
+        if (prime[p]==True):
+            for i in range(p*p,num+1,p):
+                prime[i]=False
+
+        p+=1
+
+    for p in range(2,num+1):
+        if prime[p]:
+            print(p)
+
+num=int(input("Enter the number"))
+print(f"Following are the prime numbers smaller than or equal to {num}")
+sieveoferasthoses(num)
